e625935b97a946a2f68ea30fe4ca59644f0d45d1
Daniel-Ax <finywow@gmail.com>
2021-06-16 22:02:27 +0200

    added db, appconfig, sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+import datetime
 
-app=Flask(__name__)
+from flask import Flask, render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    completed=db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    completed = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route("/")
 def init():
@@ -14,3 +28,5 @@ def who():
 @app.route("/hi/<username>")
 def greet(username):
     return f"Hi there, {username}!"
+
+sqlalchemy.__version__
