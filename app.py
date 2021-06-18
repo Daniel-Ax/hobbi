476d9b45f6feb24bfe0e2507d0cc340a3045e577
Daniel-Ax <finywow@gmail.com>
2021-06-18 21:47:33 +0200

    db create

--- app.py
+++ app.py
@@ -1,25 +1,69 @@
-import datetime
-import os
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-file_path = os.path.abspath(os.getcwd())+"\database.db"
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Users/Dani/PycharmProjects/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    completed=db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    completed = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    content = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return '<Reservation %r>' % self.id
 
-@app.route('/',methods=['POST','GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    db.create_all()
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = User(task_content)
 
-if __name__=="__main__":
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your reservation'
+
+    else:
+        tasks = User.query.order_by(User.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = User.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that reservation'
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = User.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating reservation'
+
+    else:
+        return render_template('update.html', task=task)
+
+
+if __name__ == "__main__":
+
     app.run(debug=True)
